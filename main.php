f1a184423255f452ead801c492324117c8f2928b
Reotech <richierex10@gmail.com>
2023-07-21 11:34:57 -0700

    worked on resizing various file types at once

--- main.php
+++ main.php
@@ -12,8 +12,54 @@ foreach ($files['name'] as $key => $val) {
 
     // Check if file type is valid 
     $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
-    if (!in_array($fileType, $allowTypes)) {
+
+    if (!in_array($fileType, $allowedTypes)) {
         echo 'Wrong file extension';
         return;
+    }
+
+    if ($fileType == 'jpg' || $fileType == 'jpeg') {
+        if (move_uploaded_file($files["tmp_name"][$key], $targetFilePath)) {
+            $resized = imagescale(imagecreatefromjpeg("uploads/" . $fileName), $width, $height);
+            $background = imagecolorallocate($resized, 0, 0, 0);
+
+            imagecolortransparent($resized, $background);
+            imagealphablending($resized, false);
+            imagesavealpha($resized, true);
+            imagejpeg($resized, 'uploads/' . $fileName);
+
+            // echo 'Successful';
+            // return;
+        }
+    }
+
+    if ($fileType == 'gif') {
+        if (move_uploaded_file($files["tmp_name"][$key], $targetFilePath)) {
+            $resized = imagescale(imagecreatefromgif("uploads/" . $fileName), $width, $height);
+            $background = imagecolorallocate($resized, 0, 0, 0);
+
+            imagecolortransparent($resized, $background);
+            imagealphablending($resized, false);
+            imagesavealpha($resized, true);
+            imagegif($resized, 'uploads/' . $fileName);
+
+            // echo 'Successful';
+            // return;
+        }
+    }
+
+    if ($fileType == 'png') {
+        if (move_uploaded_file($files["tmp_name"][$key], $targetFilePath)) {
+            $resized = imagescale(imagecreatefrompng("uploads/" . $fileName), $width, $height);
+            $background = imagecolorallocate($resized, 0, 0, 0);
+
+            imagecolortransparent($resized, $background);
+            imagealphablending($resized, false);
+            imagesavealpha($resized, true);
+            imagepng($resized, 'uploads/' . $fileName);
+
+            // echo 'Successful';
+            // return;
+        }
     }
 }
